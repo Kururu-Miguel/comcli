cf65c0aa01c1a5a2266dd6dddb01068a0f510e37
kururu_miguel <justmike9139@gmail.com>
2026-04-15 20:48:58 -0400

    estrutura básica do programa feita, aparentemente

---
+++ comcli/handler.py
@@ -0,0 +1,13 @@
+from cliente import main as cliente_main
+from tui import main as tui_main
+
+
+
+def main():
+    print(f'Cliente: {cliente_main()}\nTui: {tui_main()}')
+
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ comcli/servidor.py
@@ -0,0 +1,2 @@
+def main(port):
+    print(f'servidor iniciado em: ws://localhost:{port}')
